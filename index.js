fb09966d1f82dfa48272a571598770a2464e4946
gich2009 <waneneamos@gmail.com>
2023-04-03 16:05:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Add your code here
+function submitData(userName, userEmail){
+  const baseURL = "http://localhost:3000";
+  const basePath = "/users";
+
+  const dataObject = {
+    name: userName,
+    email: userEmail
+  }
+
+  const configurationObject = {
+    method: "POST",
+
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+
+    body: JSON.stringify(dataObject)
+  };
+
+  const destinationURL = baseURL + basePath;
+
+  return fetch (destinationURL, configurationObject)
+  .then((response) => response.json())
+  .then((data) => {
+    let newId = data.id;
+    document.body.innerHTML = newId;
+  })
+  .catch((error) => {
+    document.body.innerHTML = error.message;
+  })
+}
